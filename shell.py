e6d3b676f2b793cf775d009d956d352abf56dc90
vm15717 <vm15717@my.bristol.ac.uk>
2023-04-13 12:09:24 +0100

    fixed a typo in line 7 in shell.py

--- shell.py
+++ shell.py
@@ -4,6 +4,6 @@ while True:
     text = input("excalibur>")
     result, error = excalibur.run_excalibur(text)
     if error:
-        print(str(error.as_string))
+        print(str(error.as_string()))
     else:
         print(result)
