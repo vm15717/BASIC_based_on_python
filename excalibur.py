1d0cda6e29d2bcdee1dec111f5ad383f72236d07
vm15717 <vm15717@my.bristol.ac.uk>
2023-04-13 12:28:09 +0100

    finished making the Lexer. A new position class has been added that keeps track of the position.

--- excalibur.py
+++ excalibur.py
@@ -4,18 +4,43 @@ DIGITS = '0123456789'
 
 
 class Error:
-    def __init__(self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
 
     def as_string(self):
-        result = f'{self.error_name}: {self.details}'
+        result = f'{self.error_name}: {self.details}' + '\n'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
 
 
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+# POSITION
+
+
+class Position():
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.txt = ftxt
+
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.txt)
 
 
 # TOKENS
@@ -41,16 +66,17 @@ class Token:
 
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
 
     def advance(self):
-        self.pos += 1
-        if self.pos < len(self.text):
-            self.current_char = self.text[self.pos]
+        self.pos.advance(self.current_char)
+        if self.pos.idx < len(self.text):
+            self.current_char = self.text[self.pos.idx]
         else:
             self.current_char = None
 
@@ -81,9 +107,11 @@ class Lexer:
                 self.advance()
             else:
                 # return some error
+                pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError("'" + char + "'")
+                return [], IllegalCharError(pos_start, self.pos, "'" + char
+                                            + "'")
         return tokens_list, None
 
     def make_number(self):
@@ -104,7 +132,7 @@ class Lexer:
             return Token(TT_FLOAT, float(num_str))
 
 
-def run_excalibur(text):
-    lexer = Lexer(text)
+def run_excalibur(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
     return tokens, error
--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import excalibur
 
 while True:
     text = input("excalibur>")
-    result, error = excalibur.run_excalibur(text)
+    result, error = excalibur.run_excalibur('<stdin>', text)
     if error:
         print(str(error.as_string()))
     else:
